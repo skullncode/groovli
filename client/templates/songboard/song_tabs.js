1b91efae69573961d1d6f5f9b72bc6e13e541d75
skullncode <sandjay@gmail.com>
2015-05-16 20:38:36 +0530

    fixed my groovs auto scroll error and cleaned up code a bit

--- client/templates/songboard/song_tabs.js
+++ client/templates/songboard/song_tabs.js
@@ -1,18 +1,18 @@
 Template.songTabs.events({
 	'click #songTabs a[href="#mygroovs"]': function (event) {
 		//console.log('CLICKED My Groovs TAB!');
-		Session.set('activeTab', 'me');
+		//Session.set('activeTab', 'me');
 		return true;
 	},
 	'click #songTabs a[href="#tastemakers"]': function (event) {
 		//console.log('CLICKED TASTEMAKERS TAB!');
-		Session.set('activeTab', 'friends');
+		//Session.set('activeTab', 'friends');
 		//Meteor.call('getSongsOfFriends');
 		return true;
 	},
 	'click #songTabs a[href="#global"]': function (event) {
 		//console.log('CLICKED Global TAB!');
-		Session.set('activeTab', 'global');
+		//Session.set('activeTab', 'global');
 		return true;
 	}
 });
