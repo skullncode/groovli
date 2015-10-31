1080bcfa719b7e7d77cbf40ec9a58004ed502d08
skullncode <sandjay@gmail.com>
2015-10-31 16:55:37 +0530

    created a modal feedback form for users to send their feedback directly to me via email

--- client/templates/layouts/semanticFooter.js
+++ client/templates/layouts/semanticFooter.js
@@ -13,10 +13,56 @@ Template.semanticFooter.helpers({
 });
 
 Template.semanticFooter.events({
-    "click #aboutFooterLink": function (event) {
-      console.log('CLICKED ABOUT link');
+    /*"click #aboutFooterLink": function (event) {
+      //console.log('CLICKED ABOUT link');
       $('.ui.modal.aboutPage').modal({
+          transition: 'horizontal flip'
+      }).modal('show');
+    },*/
+    "click #feedbackForm": function(event) {
+      //console.log('clicked feedback link!!!');
+      $('.ui.modal.feedbackForm').modal({
+          onHide: function () {
+            //console.log("HIDING modal now!");
+              //resetFlylistForm();
+              //updateFlylistHeaderTextAccordingly('hide');
+              //Session.set('flistM', '');
+          },
+          onShow: function() {
+            resetFeedbackForm();
+          },
+          onDeny: function(){
+          //console.log('CLICKED CANCEL!!!');
+          return true;
+        },
+          onApprove: function () {
+            //console.log('CLICKED SUBMIT FEEDBACK!!!');
+            return validateFeedbackForm();
+          },
           transition: 'horizontal flip'
       }).modal('show');
     }
 });
+
+function validateFeedbackForm() {
+  if($('#feedbackType:checked').length == 0 || _.isEmpty($('#txtFeedback').val()) || _.isEmpty($('#userEmail').val()))
+    return false;
+  else if($('#feedbackType:checked').length == 1 && !_.isEmpty($('#txtFeedback').val()) && !_.isEmpty($('#userEmail').val()))
+  {
+    Meteor.call('sendFeedback', $('input[name=feedbackType]:checked', '#modalFeedbackForm').val(), $('#txtFeedback').val(), $('#userEmail').val(), function(error) {
+        if (error) {
+          return console.log(error);
+        } else {
+          //return alert("Thank you for your valuable feedback!");
+          return toastr.success("Thank you for your valuable feedback!");
+        }
+      });
+    return true;
+  }
+}
+
+function resetFeedbackForm(){
+  $('input[name=feedbackType]:checked', '#modalFeedbackForm').prop('checked',false);
+  $('#txtFeedback').val('');
+  $('#userEmail').val('');
+}
--- server/methods/feedback.js
+++ server/methods/feedback.js
@@ -3,11 +3,11 @@ Meteor.methods({
     console.log('THis is the feedbackType: ' + feedbackType);
     console.log('THis is the feedback Text: ' + feedbackText);
     console.log('THis is the user email: ' + userEmail);
-    console.log('THis is the siteSpeed: ' + siteSpeed);
+    //console.log('THis is the siteSpeed: ' + siteSpeed);
     Email.send({
       to: "sandeep@groovli.com",
       from: userEmail,
-      subject: "Groovli Contact Form - " + feedbackType,
+      subject: "Groovli Feedback Form - " + feedbackType,
       html: Handlebars.templates['send-feedback']({
         type: feedbackType,
         detailedText: feedbackText,
