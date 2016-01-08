e7edee8095e4eaf2e5a4779249087559a43abf3b
skullncode <sandjay@gmail.com>
2016-01-08 11:02:26 +0530

    added email address column to user master list

--- client/templates/layouts/semanticFooter.js
+++ client/templates/layouts/semanticFooter.js
@@ -11,7 +11,7 @@ Template.semanticFooter.helpers({
 	 }
   },
   siteLastUpdatedTimestamp: function() {
-    var lastUpdatedManualTimestamp = new moment("December 21st 2015, 4:30 pm", "'MMMM Do YYYY, h:mm a'");
+    var lastUpdatedManualTimestamp = new moment("January 8th 2016, 9:36 am", "'MMMM Do YYYY, h:mm a'");
     return new moment(lastUpdatedManualTimestamp).fromNow();
   }
 });
